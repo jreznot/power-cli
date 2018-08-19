dd44ea7634b401a462d4ad7d6f34d02f5755e5ca
Yuriy Artamonov <artamonov@haulmont.com>
2018-08-19 23:12:15 +0400

    Demo commands

--- link.sh
+++ link.sh
@@ -1,19 +1,26 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-$JAVA_HOME/bin/jlink --module-path build/libs:$JAVA_HOME/jmods --add-modules com.haulmont.powercli --launcher \
-  launch=com.haulmont.powercli/com.haulmont.powercli.Application --output build/dist \
+$JAVA_HOME/bin/jlink --module-path build/libs:$JAVA_HOME/jmods --add-modules com.haulmont.demos.powercli --launcher \
+  launch=com.haulmont.demos.powercli/com.haulmont.demos.powercli.Application --output build/dist \
   --strip-debug --compress 2 --no-header-files --no-man-pages
 
 # with guava
 
-$JAVA_HOME/bin/jlink --module-path build/install/power-cli/lib:$JAVA_HOME/jmods --add-modules com.haulmont.powercli --launcher \
-  launch=com.haulmont.powercli/com.haulmont.powercli.Application --output build/dist \
+$JAVA_HOME/bin/jlink --module-path build/install/power-cli/lib:$JAVA_HOME/jmods \
+  --add-modules com.haulmont.demos.powercli --launcher \
+  launch=com.haulmont.demos.powercli/com.haulmont.demos.powercli.Application --output build/dist \
   --strip-debug --compress 2 --no-header-files --no-man-pages
 
 # jdeps
 
-$JAVA_HOME/bin/jdeps --module-path . --generate-module-info . guava-25.0-jre.jar
+$JAVA_HOME/bin/jdeps --module-path . --generate-module-info . guava-26.0-jre.jar
 
 # patch modules
 
-$JAVA_HOME/bin/javac -p build/install/power-cli/lib -d build/modules --patch-module com.google.common=build/install/power-cli/lib/guava-25.0-jre.jar bundles/com.google.common/module-info.java
+$JAVA_HOME/bin/javac -p build/install/power-cli/lib -d build/modules \
+  --patch-module com.google.common=build/install/power-cli/lib/guava-26.0-jre.jar \
+  bundles/com.google.common/module-info.java
+
+# jar patch
+
+$JAVA_HOME/bin/jar uf build/install/power-cli/lib/guava-26.0-jre.jar -C build/modules module-info.class
