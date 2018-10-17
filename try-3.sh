b78645c36165ce044680f6de9ededb462478da67
Yuriy Artamonov <artamonov@haulmont.com>
2018-10-17 18:28:46 +0400

    Try scripts

--- try-3.sh
+++ try-3.sh
@@ -2,7 +2,7 @@
 
 # with guava
 
-$JAVA_HOME/bin/jlink --module-path build/libs:$JAVA_HOME/jmods \
+$JAVA_HOME/bin/jlink --module-path build/install/power-cli/lib:$JAVA_HOME/jmods \
   --add-modules com.haulmont.demos.powercli --launcher \
   launch=com.haulmont.demos.powercli/com.haulmont.demos.powercli.Application --output build/dist \
   --strip-debug --compress 2 --no-header-files --no-man-pages
--- try-simple.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-$JAVA_HOME/bin/jlink --module-path build/libs:$JAVA_HOME/jmods --add-modules com.haulmont.demos.powercli --launcher \
-  launch=com.haulmont.demos.powercli/com.haulmont.demos.powercli.Application --output build/simple \
-  --strip-debug --compress 2 --no-header-files --no-man-pages
